5f0cc72eb47f097a37526403a5a1f6418758f697
MahdiHaeri <mahdihaerim@gmail.com>
2024-04-09 02:52:57 +0330

    Add badge to avatar in navDrawer header

--- src/layouts/dashboard/navDrawer/component/DrawerHeader.jsx
+++ src/layouts/dashboard/navDrawer/component/DrawerHeader.jsx
@@ -1,4 +1,4 @@
-import {Avatar, Button, Icon, IconButton, Stack, Typography} from "@mui/material";
+import {Avatar, Badge, Button, Icon, IconButton, Stack, Typography} from "@mui/material";
 import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
 import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
 import ViewSidebarRoundedIcon from '@mui/icons-material/ViewSidebarRounded';
@@ -8,8 +8,23 @@ export const DrawerHeader = ({openNav, onToggleDrawer}) => {
         <Stack direction={"row"} alignItems={"center"} justifyContent={"space-between"}>
             <Button variant={"text"}>
                 <Stack direction={"row"} alignItems={"center"} justifyContent={"center"} spacing={1}>
-                    <Avatar sx={{width: 20, height: 20, outline: '2px solid', outlineColor: 'primary.main', outlineOffset: 1}}>
-                    </Avatar>
+                    <Badge
+                        color={'secondary'}
+                        badgeContent={' '}
+                        variant={'dot'}
+                        overlap={'rectangular'}
+                        anchorOrigin={{vertical: 'top', horizontal: 'right'}}
+                        invisible={false}
+                    >
+                        <Avatar sx={{
+                            width: 20,
+                            height: 20,
+                            outline: '2px solid',
+                            outlineColor: 'primary.main',
+                            outlineOffset: 1
+                        }}>
+                        </Avatar>
+                    </Badge>
                     <Stack direction={"row"} alignItems={"center"} justifyContent={"center"}>
                         <Typography variant={"body2"} fontWeight={"bold"}>
                             Mahdi
