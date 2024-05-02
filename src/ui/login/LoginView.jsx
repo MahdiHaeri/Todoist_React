6d0fa862729b4b4e3ec7303f7137d320c64a474f
MahdiHaeri <mahdihaerim@gmail.com>
2024-05-02 14:32:02 +0330

    Update Login page

--- src/ui/login/LoginView.jsx
+++ src/ui/login/LoginView.jsx
@@ -1,4 +1,4 @@
-import {Button, Stack, TextField, Typography} from "@mui/material";
+import {Button, Divider, Stack, TextField, Typography} from "@mui/material";
 import {GoogleSvgIcon} from "../../assets/GoogleSvgIcon.jsx";
 import {FacebookSvgIcon} from "../../assets/FacebookSvgIcon.jsx";
 import {AppleSvgIcon} from "../../assets/AppleSvgIcon.jsx";
@@ -17,49 +17,108 @@ export const LoginView = () => {
         >
             <Stack
                 direction='column'
-                width='30%'
+                width='35%'
                 height={'100%'}
-                gap={4}
+                gap={3}
             >
                 <Typography variant='h4' fontWeight='bold'>
                     Log in
                 </Typography>
                 <Stack direction='column' gap={1}>
-                    <Button variant="outlined" startIcon={
-                        <GoogleSvgIcon/>
-                    } color={'inherit'}>
-                        Continue with Goggle
+                    <Button
+                        variant="outlined"
+                        size={"large"}
+                        startIcon={<GoogleSvgIcon/>}
+                        color={'inherit'}
+                        sx={{
+                            borderColor: 'divider',
+                            textTransform: 'none'
+                        }}
+                    >
+                        <Typography variant={"subtitle1"} fontWeight={"bold"}>
+                            Continue with Google
+                        </Typography>
                     </Button>
-                    <Button variant="outlined" startIcon={
-                        <FacebookSvgIcon/>
-                    } color={'inherit'}>
-                        Continue with FaceBook
+                    <Button
+                        variant="outlined"
+                        size={"large"}
+                        startIcon={<FacebookSvgIcon/>}
+                        color={'inherit'}
+                        sx={{
+                            borderColor: 'divider',
+                            textTransform: 'none'
+                        }}
+                    >
+                        <Typography variant={"subtitle1"} fontWeight={"bold"}>
+                            Continue with FaceBook
+                        </Typography>
                     </Button>
-                    <Button variant="outlined" startIcon={
-                        <AppleSvgIcon/>
-                    } color={'inherit'}>
-                        Continue with Apple
+                    <Button
+                        variant="outlined"
+                        size={"large"}
+                        startIcon={<AppleSvgIcon/>}
+                        color={'inherit'}
+                        sx={{
+                            borderColor: 'divider',
+                            textTransform: 'none'
+                        }}
+                    >
+                        <Typography variant={"subtitle1"} fontWeight={"bold"}>
+                            Continue with Apple
+                        </Typography>
                     </Button>
                 </Stack>
 
+                <Divider flexItem>
+                    <Typography variant={"subtitle2"} sx={{color: 'text.secondary'}}>
+                        Login with Email
+                    </Typography>
+                </Divider>
+
                 <Stack direction='column' gap={1}>
-                    <TextField variant={'outlined'} label={'Email'} type={'email'}/>
-                    <TextField variant={'outlined'} label={'Password'} type={'password'}/>
-                    <Button variant="contained" color='primary' disableElevation>
-                        <Typography variant="body1" fontWeight={"bold"}>
+                    <TextField
+                        variant={'outlined'}
+                        size={"small"}
+                        label={'Email'}
+                        type={'email'}
+                    />
+                    <TextField
+                        variant={'outlined'}
+                        size={"small"}
+                        label={'Password'}
+                        type={'password'}
+                    />
+                    <Button
+                        variant="contained"
+                        size={"large"}
+                        color='primary'
+                        disableElevation
+                        sx={{
+                            textTransform: 'none'
+                        }}
+                    >
+                        <Typography variant="subtitle1" fontWeight={"bold"}>
                             Log in
                         </Typography>
                     </Button>
-                    <Link to={'#'}>
-                        Forgot your password?
-                        <link/>
+                    <Link
+                        to={'#'}
+                        style={{
+                            color: 'inherit',
+                        }}
+                    >
+                        <Typography variant={"subtitle2"}>
+                            Forgot your password?
+                        </Typography>
                     </Link>
                 </Stack>
 
 
                 <Typography variant="subtitle2">
-                    By continuing with Google, Apple, or Email, you agree to Todoist’s
-                    Terms of Service and Privacy Policy.
+                    By continuing with Google, Apple, or Email, you agree to Todoist’s <Link to={'#'}>Terms of Service</Link> and <Link to={'#'}>Privacy Policy</Link> .
+                </Typography>
+                <Typography variant="subtitle2" textAlign={"center"}>
+                    Don’t have an account? <Link to={'#'}>Sign up</Link>
                 </Typography>
             </Stack>
         </Stack>
