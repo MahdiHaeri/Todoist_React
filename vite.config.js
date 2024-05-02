6822ca900a7dadc7a06ac7ef212c0a2e6573c3eb
MahdiHaeri <mahdihaerim@gmail.com>
2024-05-02 14:48:10 +0330

    Update Vite confing to run on port 3000

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 3000,
+  },
   plugins: [react()],
 })
